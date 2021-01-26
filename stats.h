e1c030877acc7e60874583daab34bd6597ec4f37
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-01-26 16:22:05 +0530

    Update stats.h
    Added function declaration

--- stats.h
+++ stats.h
@@ -12,5 +12,7 @@ typedef struct Stats  {
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
+Stat compute_statistics(float numberset[], int setlength); 
+
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
