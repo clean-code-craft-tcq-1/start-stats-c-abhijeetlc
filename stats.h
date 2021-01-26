6d10ae58b05cf8acb4cfb2a1dfca75f75dc894b5
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-01-26 16:20:12 +0530

    Update stats.h
    Added structure definition

--- stats.h
+++ stats.h
@@ -1,6 +1,14 @@
 
 //struct Stats compute_statistics(float numberset[], int setlength);
 
+typedef struct Stats  {
+    //struct Stats s;
+    float average = 0;
+    float min = 0;
+    float max = 0;
+    
+}Stat;
+
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
