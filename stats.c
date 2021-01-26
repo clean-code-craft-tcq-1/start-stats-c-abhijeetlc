67c2e35db216cf8ef70785595e29eaf8bab0764c
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-01-27 01:24:38 +0530

    Update stats.c
    Corrected function definition.

--- stats.c
+++ stats.c
@@ -17,37 +17,22 @@ Stat compute_statistics(float numberset[], int setlength)
     float sum = 0, min = 0, max = 0 ;
     Stat Statcalc;
     
-    for (i=0;i<setlength ;i++)
+    for (i=0;i<setlength;i++)
     {
            sum+=numberset[i];
     }
     
-    for (i=0;i<setlength-1;i++)
+    for (i=0;i<setlength;i++)
     {
-        if (numberset[i]<numberset[i+1] )
+        if (numberset[i]<min)
         {
             min=numberset[i];
         }
     }
     
-    for (i=0;i<setlength-1;i++)
+    for (i=0;i<setlength;i++)
     {
-        if (numberset[i]>numberset[i+1] )
-        {
-            max=numberset[i];
-        }
-    }
-	
-    //Statcalc.average = (sum/setlength);
-    //Statcalc.min = min ;
-    //Statcalc.max =  max;  
-    
-   //return Statcalc ;
-	
-  //} 
-    for (i=0;i<setlength-1;i++)
-    {
-        if (numberset[i]>numberset[i+1] )
+        if (numberset[i]>max)
         {
             max=numberset[i];
         }
